fb38acc13b3b0433930c734b7e4c60db889e7b7a
vibhumahendru <vibhumahendru@gmail.com>
2020-10-26 15:39:40 +0530

    new fn added

--- src/App.js
+++ src/App.js
@@ -16,36 +16,95 @@ function App() {
   const [gamesGraphData, setGamesGraphData] = useState({})
   const [pointsGraphData, setPointsGraphData] = useState({})
 
-  useEffect(()=>{
-    axios.get('https://ping-pong-score.herokuapp.com/get_score/')
-    .then(res => setTableData(res.data))
-    .catch()
+  const [currentSession, setCurrentSession] = useState({})
+  const [vibhuScore, setVibhuScore] = useState(0)
+  const [dadScore, setDadScore] = useState(0)
+  const [apiKey, setApiKey] = useState('')
 
-    axios.get('https://ping-pong-score.herokuapp.com/get_graph_data/')
-    .then((res) => {
-      let gamesData = [
-        {
-        "name":"Vibhu", "data":res.data.vibhu_games_graph,
-        },
-        {
-        "name":"Dad", "data":res.data.dad_games_graph,
-        },
-    ]
-      let pointsData = [
-        {
-        "name":"Vibhu", "data":res.data.vibhu_points_graph,
-        },
-        {
-        "name":"Dad", "data":res.data.dad_points_graph,
-        },
-    ]
-      setGamesGraphData(gamesData)
-      setPointsGraphData(pointsData)
+const getScore = () =>{
+  // axios.get('https://ping-pong-score.herokuapp.com/get_score/')
+  axios.get('http://127.0.0.1:8000/get_score/')
+  .then(res => setTableData(res.data))
+  .catch()
+}
+
+const getGraphData = () =>{
+  // axios.get('https://ping-pong-score.herokuapp.com/get_graph_data/')
+  axios.get('http://127.0.0.1:8000/get_graph_data/')
+  .then((res) => {
+    let gamesData = [
+      {
+      "name":"Vibhu", "data":res.data.vibhu_games_graph,
+      },
+      {
+      "name":"Dad", "data":res.data.dad_games_graph,
+      },
+  ]
+    let pointsData = [
+      {
+      "name":"Vibhu", "data":res.data.vibhu_points_graph,
+      },
+      {
+      "name":"Dad", "data":res.data.dad_points_graph,
+      },
+  ]
+    setGamesGraphData(gamesData)
+    setPointsGraphData(pointsData)
+})
+  .catch()
+}
+const getCurrentSession = () =>{
+  // axios.get('https://ping-pong-score.herokuapp.com/get_graph_data/')
+  axios.get('http://127.0.0.1:8000/get_current_session/')
+  .then(res => {
+    setCurrentSession(res.data)
   })
-    .catch()
+  .catch()
+}
 
-
+  useEffect(()=>{
+    getScore()
+    getGraphData()
+    getCurrentSession()
   },[])
+
+
+
+  const createNewSession = () =>{
+    let makeSession = window.confirm('are you sure?')
+    if (makeSession) {
+      let data = {
+        date: new Date().toISOString().slice(0,10)
+      }
+      axios.post(`http://127.0.0.1:8000/session/?apiKey=${apiKey}`, data)
+      .then((res) => {
+        setCurrentSession(res.data)
+
+        getScore()
+        getGraphData()
+        getCurrentSession()
+      })
+      .catch(err => alert('please enter the correct api key'))
+    }
+  }
+
+  const createNewGame = () =>{
+    let data = {
+      session: currentSession.current_session_id,
+      vibhu_score: vibhuScore,
+      dad_score: dadScore,
+    }
+    axios.post(`http://127.0.0.1:8000/game/?apiKey=${apiKey}`, data)
+    .then((res) => {
+      setVibhuScore(0)
+      setDadScore(0)
+
+      getScore()
+      getGraphData()
+      getCurrentSession()
+    })
+    .catch(err => alert('please enter the correct api key'))
+  }
 
   return (
     <div className="App">
@@ -84,6 +143,44 @@ function App() {
     : "Loading Data"}
     <LineChart legend={false} ytitle="Games won"  data={gamesGraphData} />
     <LineChart ytitle="Points won" legend="bottom" data={pointsGraphData} />
+      <Table style={{"minWidth":"350px","width":"50%"}}striped bordered hover>
+      <thead>
+        <tr>
+          <th>Current Session</th>
+          <th>{currentSession.current_session_date}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Vibhu</td>
+          <td>{currentSession.vibhu_score}</td>
+        </tr>
+        <tr>
+          <td>Dad</td>
+          <td>{currentSession.dad_score}</td>
+        </tr>
+        <tr>
+        <td colspan="2">New Game</td>
+        </tr>
+        <tr>
+          <td>vibhu score</td>
+          <td><input onChange={(e)=>setVibhuScore(e.target.value)} value={vibhuScore} type='number' min="0" ></input></td>
+        </tr>
+        <tr>
+          <td>dad score</td>
+          <td><input onChange={(e)=>setDadScore(e.target.value)} value={dadScore} type='number' min="0"></input></td>
+        </tr>
+        <tr>
+          <td>Api Key</td>
+          <td><input onChange={(e)=>setApiKey(e.target.value)}></input></td>
+          <td><button onClick={createNewGame} >save</button></td>
+        </tr>
+        <tr>
+          <td><button onClick={createNewSession} >Create New Session</button></td>
+        </tr>
+
+      </tbody>
+      </Table>
     </div>
   );
 }
